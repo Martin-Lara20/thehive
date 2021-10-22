774f846076fa8ce697f6b886efb4051ed1a8de6d
Martin-Lara20 <iinf18.mlaral@itesco.edu.mx>
2021-10-22 08:10:34 -0500

    cambio insignificante

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const {checkApiKey} = require('../middleware/auth.handler')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
+router.get('/', checkApiKey, (req, res) => {
   res.json([
     {
       description: "Welcome to THE HIVE! This is a community where all build a hive of pictures"
